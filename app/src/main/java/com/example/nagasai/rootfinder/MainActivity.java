ef4f7e9c93877c8c6c98d1030dea895a4e611ec8
nagasai <nagasai.g9@gmail.com>
2017-05-15 11:20:46 +0530

    replaced recursion with a for-loop, inproved accuracy, UI tuned slightly

--- app/src/main/java/com/example/nagasai/rootfinder/MainActivity.java
+++ app/src/main/java/com/example/nagasai/rootfinder/MainActivity.java
@@ -8,22 +8,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    static int counter = 0;
     EditText et1, et2;
     TextView tvOutput, tvHint;
     Button btnCalculate;
+    double num, root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         et1 = (EditText) findViewById(R.id.et1);
         et2 = (EditText) findViewById(R.id.et2);
-
         tvOutput = (TextView) findViewById(R.id.tvOutput);
         tvHint = (TextView) findViewById(R.id.tvHint);
-
         btnCalculate = (Button) findViewById(R.id.btnCalculate);
 
         btnCalculate.setOnLongClickListener(new View.OnLongClickListener() {
@@ -33,6 +32,7 @@ public class MainActivity extends Activity {
                 et2.setText("");
                 tvOutput.setText("");
                 tvHint.setText("");
+                counter = 0;
                 return true;
             }
         });
@@ -42,11 +42,33 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 if (et1.getText().toString().equals("") && et2.getText().toString().equals("")) {
                     tvOutput.setText(R.string.enter_numbers);
+                } else if (et2.getText().toString().equals(0)) {
+                    tvOutput.setText(String.valueOf(1));
                 } else {
-                    tvOutput.setText(String.valueOf(Math.pow(Double.parseDouble(et1.getText().toString()), (1 / Double.parseDouble(et2.getText().toString())))));
+                    num = Double.parseDouble(et1.getText().toString());
+                    root = Double.parseDouble(et2.getText().toString());
+                    rootFinder(0, num);
                 }
                 tvHint.setText(R.string.clear_values);
             }
         });
+    }
+
+    void rootFinder(double start, double end) {
+        double number, someX, sendBack = (start + end) / 2;
+        for (int i = 0; i < 8192; i++) {
+            sendBack = (start + end) / 2;
+            number = (start + end) / 2;
+            someX = Math.pow(number, root);
+            if (someX == num) {
+                sendBack = number;
+                i = 8192;
+            } else if (someX < num) {
+                start = (start + end) / 2;
+            } else if (someX > num) {
+                end = (start + end) / 2;
+            }
+        }
+        tvOutput.setText(String.valueOf(sendBack));
     }
 }
